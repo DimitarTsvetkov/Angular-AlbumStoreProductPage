4d9813fcedc2b7df0d0a8df891894e7f81cbb459
Tsvetkov <dimitar.tsvetkov@accenture.com>
2019-06-04 12:00:33 +0300

    5

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs/Observable';
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
-import { map, } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 
 @Injectable({
@@ -11,10 +11,10 @@ export class ProductService {
 
   private _albumUrl = '../assets/album.json';
 
-  constructor(private http: Http, private response: Response) { }
+  constructor(private _http: Http, private response: Response) { }
 
   getAlbum(id: number): any {
-    return this.http.get(this._albumUrl).map(response => response.json())
+    return this._http.get(this._albumUrl).map( (response) => response.json())
 
 
   }
