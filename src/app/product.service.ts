f4e29f55e85f7c86e3e8f243ac8bbb34e456bcfa
Tsvetkov <dimitar.tsvetkov@accenture.com>
2019-06-04 17:14:40 +0300

    17

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,7 @@ import { Product } from './product'
 export class ProductService {
 
   private _albumUrl = '../assets/album.json';
-  private _productsUrl: '../assets/products.json' ;
+  private _productsUrl = '../assets/products.json' ;
 
   constructor(private _http: Http) { }
 
@@ -25,6 +25,6 @@ export class ProductService {
 
   getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl).map( (response) => <Product[]>response.json());
-
+    
   }
 }
